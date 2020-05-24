06d43b6bb8e1ce95349d82ab06af87342acc4741
WnBoy <15029366160@163.com>
2020-05-24 10:34:04 +0800

    tt

--- Algorithm/src/com/wnlife/simple/PalindromeNumber.java
+++ Algorithm/src/com/wnlife/simple/PalindromeNumber.java
@@ -34,10 +34,14 @@ public class PalindromeNumber {
             return false;
         }
         int reverse=0;
+        //将数的后一半翻转，和前一半做对比
         while(x>reverse){
             reverse=reverse*10+x%10;
             x/=10;
         }
+        // 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
+        // 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
+        // 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
         return x==reverse||x==reverse/10;
     }
 }
